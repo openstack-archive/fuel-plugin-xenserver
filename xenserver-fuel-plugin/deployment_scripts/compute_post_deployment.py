8d8fc570b9e069edd48e17741ccb533e5081e826
John Hua <john.hua@citrix.com>
2015-07-29 14:20:52 +0800

    Fix post deployment tasks

--- xenserver-fuel-plugin/deployment_scripts/compute_post_deployment.py
+++ xenserver-fuel-plugin/deployment_scripts/compute_post_deployment.py
@@ -9,7 +9,29 @@ from shutil import rmtree
 from tempfile import mkstemp, mkdtemp
 
 LOG_FILE = '/tmp/compute_post_deployment.log'
+ASTUTE_PATH = '/etc/astute.yaml'
+ACCESS_SECTION = 'xenserver_access'
+XENAPI_URL = 'https://pypi.python.org/packages/source/X/XenAPI/XenAPI-1.2.tar.gz'
+
 logging.basicConfig(filename=LOG_FILE,level=logging.DEBUG)
+
+def get_access(astute_path, access_section):
+	if not os.path.exists(astute_path):
+		warning('%s not found' % astute_path)
+		return None
+
+	astute = yaml.load(open(astute_path))
+	if not access_section in astute:
+		warning('%s not found' % access_section)
+		return None
+
+	access = astute[access_section]
+	info('username: {user}'.format(**access))
+	info('password: {password}'.format(**access))
+	return access
+
+def install_xentools():
+	os.system('mount /dev/cdrom /mnt && /mnt/Linux/install.sh')
 
 def init_eth(dev_no):
 	fname = '/etc/network/interfaces.d/ifcfg-eth%d' % (dev_no)
@@ -20,20 +42,6 @@ iface eth2 inet dhcp
 	with open(fname, 'w') as f:
 		f.write(s)
 	info('%s created' % fname)
-
-def install_xen_tools():
-	#TODO
-	'''
-	local xen_tools_url
-	xen_tools_url="$1"
-
-	local xen_tools_file
-	xen_tools_file=$(mktemp)
-
-	wget -qO "$xen_tools_file" "$xen_tools_url"
-	dpkg -i "$xen_tools_file"
-	rm "$xen_tools_file"
-	'''
 
 def install_xenapi_sdk(xenapi_url):
 	xenapi_zipball = mkstemp()[1]
@@ -57,35 +65,23 @@ def install_xenapi_sdk(xenapi_url):
 	os.remove(xenapi_zipball)
 	rmtree(xenapi_sources)
 
-def create_novacompute_conf(fuel_plugin_name='xenserver-fuel-plugin'):
-	astute_path = '/etc/astute.yaml'
-	if not os.path.exists(astute_path):
-		warning('%s not found' % astute_path)
-		return
-
-	astute = yaml.load(open(astute_path))
-	if not fuel_plugin_name in astute:
-		warning('%s not found in %s' % (fuel_plugin_name, astute_path))
-		return
-
-	env = astute[fuel_plugin_name]
-	info('username: {username_text}'.format(**env))
-	info('password: {password_text}'.format(**env))
-
+def create_novacompute_conf(access):
 	template = \
 """[DEFAULT]
 compute_driver=xenapi.XenAPIDriver
 [xenserver]
 connection_url=http://10.219.10.22
-connection_username={username_text}
-connection_password={password_text}
+connection_username={user}
+connection_password={password}
 """
-	s = template.format(**env)
+	s = template.format(**access)
 	with open('/etc/nova/nova-compute.conf','w') as f:
 		f.write(s)
+	info('nova-compute.conf created')
 
 if __name__ == '__main__':
 	init_eth(2)
-	#install_xen_tools "http://xen-tools.org/software/xen-tools/xen-tools_4.5-1_all.deb"
-	install_xenapi_sdk('https://pypi.python.org/packages/source/X/XenAPI/XenAPI-1.2.tar.gz')
-	create_novacompute_conf('xenserver-fuel-plugin')
+	install_xenapi_sdk(XENAPI_URL)
+	access = get_access(ASTUTE_PATH, ACCESS_SECTION)
+	if access is not None:
+		create_novacompute_conf(access)
