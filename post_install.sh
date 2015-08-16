0d26823206015e94d5463159be4a970ef7bd302a
huazhihao <john.hua@citrix.com>
2015-08-16 21:52:30 +0800

    Fix post install hook

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,4 @@
-name=${1-"xenserver-fuel-plugin*"}
-
+cd /var/www/nailgun/plugins/%{name}
 dockerctl copy cleardb.py nailgun:/tmp/cleardb.py
 dockerctl shell nailgun /tmp/cleardb.py
 dockerctl shell nailgun rm /tmp/cleardb.py
