811d00c461c115ad234329b3c8261f635c081284
John Hua <john.hua@citrix.com>
2015-08-21 19:10:29 +0800

    Fix post install current path

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,6 @@
+name=${1-"xenserver-fuel-plugin*"}
 cd /var/www/nailgun/plugins/%{name}
+
 dockerctl copy cleardb.py nailgun:/tmp/cleardb.py
 dockerctl shell nailgun /tmp/cleardb.py
 dockerctl shell nailgun rm /tmp/cleardb.py
