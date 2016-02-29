4aa373be9a787a59c7613ecd3c3a1c99bb1acda3
John Hua <john.hua@citrix.com>
2016-02-29 15:24:37 +0800

    Update release yaml for MOS 8.0
    * Replace original 7.0 release yaml file with 8.0
    * Patch xs_release.yaml to openstack.yaml
    
    Change-Id: I1d96a9318d4beca1179cb880bff48da6aa8e7946

--- post_install.sh
+++ post_install.sh
@@ -4,7 +4,12 @@ cd /var/www/nailgun/plugins/%{name}
 dockerctl copy cleardb.py nailgun:/tmp/cleardb.py
 dockerctl shell nailgun /tmp/cleardb.py
 dockerctl shell nailgun rm /tmp/cleardb.py
-dockerctl copy newrelease.yaml nailgun:/tmp/newrelease.yaml
-dockerctl shell nailgun manage.py loaddata /tmp/newrelease.yaml
-dockerctl shell nailgun rm /tmp/newrelease.yaml
+
+sed "$(grep -n '\- pk: 1' openstack.yaml | cut -d : -f 1),$ d" openstack.yaml > new_release.yaml
+cat xs_release.yaml >> new_release.yaml
+
+dockerctl copy new_release.yaml nailgun:/tmp/new_release.yaml
+dockerctl shell nailgun manage.py loaddata /tmp/new_release.yaml
+dockerctl shell nailgun rm /tmp/new_release.yaml
+rm new_release.yaml
 fuel rel --sync-deployment-tasks --dir /etc/puppet/
