644d88263a24ae76b24ae7d48b3de269ead970c1
John Hua <john.hua@citrix.com>
2016-03-02 18:43:43 +0800

    Upgrade release and metadata for MOS 8.0
    Change-Id: I89027968b3678d93299f1993c946e9e1994643c1

--- post_install.sh
+++ post_install.sh
@@ -4,7 +4,7 @@ cd /var/www/nailgun/plugins/%{name}
 dockerctl copy cleardb.py nailgun:/tmp/cleardb.py
 dockerctl shell nailgun /tmp/cleardb.py
 dockerctl shell nailgun rm /tmp/cleardb.py
-dockerctl copy newrelease.yaml nailgun:/tmp/newrelease.yaml
-dockerctl shell nailgun manage.py loaddata /tmp/newrelease.yaml
-dockerctl shell nailgun rm /tmp/newrelease.yaml
+dockerctl copy xs_release.yaml nailgun:/tmp/xs_release.yaml
+dockerctl shell nailgun manage.py loaddata /tmp/xs_release.yaml
+dockerctl shell nailgun rm /tmp/xs_release.yaml
 fuel rel --sync-deployment-tasks --dir /etc/puppet/
