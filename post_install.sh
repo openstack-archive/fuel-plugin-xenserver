b0ec76366cba835451243da12b3fe38f9d34e7b3
John Hua <john.hua@citrix.com>
2016-03-02 18:43:43 +0800

    Upgrade release and metadata for MOS 8.0
    * Upgrade version
    * Patch diff to original openstack.yaml
    
    Change-Id: I89027968b3678d93299f1993c946e9e1994643c1

--- post_install.sh
+++ post_install.sh
@@ -4,7 +4,11 @@ cd /var/www/nailgun/plugins/%{name}
 dockerctl copy cleardb.py nailgun:/tmp/cleardb.py
 dockerctl shell nailgun /tmp/cleardb.py
 dockerctl shell nailgun rm /tmp/cleardb.py
-dockerctl copy newrelease.yaml nailgun:/tmp/newrelease.yaml
-dockerctl shell nailgun manage.py loaddata /tmp/newrelease.yaml
-dockerctl shell nailgun rm /tmp/newrelease.yaml
+
+cp openstack.yaml xs_release.yaml
+patch -p1 xs_release.yaml < xs_release.patch
+dockerctl copy xs_release.yaml nailgun:/tmp/xs_release.yaml
+
+dockerctl shell nailgun manage.py loaddata /tmp/xs_release.yaml
+dockerctl shell nailgun rm /tmp/xs_release.yaml
 fuel rel --sync-deployment-tasks --dir /etc/puppet/
