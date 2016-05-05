3b2bae393dbf8fa4f23b9a52b8d590c09d94da79
John Hua <john.hua@citrix.com>
2016-05-05 13:24:07 +0800

    Upgrade to fpb package version 4
    Fuel plugin builder package version 4 introduces components.yaml to enable fuel
    plugin manipulate wizard and settings without creating new release. So the old
    xs_release.yaml will not be needed any more as well as other scripts or yamls
    that helps on that.
    
    Change-Id: I6e2226176faf11fff986f03da184558e6cfdce05

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-name=${1-"fuel-plugin-xenserver*"}
-cd /var/www/nailgun/plugins/%{name}
-
-dockerctl copy cleardb.py nailgun:/tmp/cleardb.py
-dockerctl shell nailgun /tmp/cleardb.py
-dockerctl shell nailgun rm /tmp/cleardb.py
-fuel rel --sync-deployment-tasks --dir /etc/puppet/
