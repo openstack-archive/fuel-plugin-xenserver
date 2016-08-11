0bd22d314e863cea285da2b3d2671ca51f50d244
John Hua <john.hua@citrix.com>
2016-08-11 16:46:38 +0800

    Build the ISO as part of the makefile
    Change-Id: Id2be52de5c8b86af1ca76cb0e70cd0ed80bac3a2

--- suppack/build-xenserver-suppack.sh
+++ suppack/build-xenserver-suppack.sh
@@ -17,7 +17,6 @@ set -eux
 
 THIS_FILE=$(readlink -f $0)
 FUELPLUG_UTILS_ROOT=$(dirname $THIS_FILE)
-DEPLOYMENT_SCRIPT_ROOT=$(dirname $FUELPLUG_UTILS_ROOT)
 cd $FUELPLUG_UTILS_ROOT
 rm -rf xenserver-suppack
 mkdir -p xenserver-suppack && cd xenserver-suppack
@@ -26,18 +25,18 @@ mkdir -p xenserver-suppack && cd xenserver-suppack
 # =============================================
 # Configurable items
 
-# xenserver version info
-PLATFORM_VERSION=${1:-"1.9"}
-XS_BUILD=${2:-"90233c"}
+# OpenStack release
+OS_RELEASE=${1:-"mitaka"}
 
-# branch info
-GITBRANCH=${3:-"origin/stable/liberty"}
+# xenserver version info
+PLATFORM_VERSION=${2:-"1.9"}
+XS_BUILD=${3:-"90233c"}
 
 # nova and neutron xenserver dom0 plugin version
 XS_PLUGIN_VERSION=${4:-"2015.1"}
 
-# OpenStack release
-OS_RELEASE=${5:-"liberty"}
+# branch info
+GITBRANCH="origin/stable/$OS_RELEASE"
 
 # repository info
 NOVA_GITREPO="https://git.openstack.org/openstack/nova"
@@ -48,6 +47,17 @@ RPM_BUILDER_REPO="https://github.com/citrix-openstack/xenserver-nova-suppack-bui
 # Update system and install dependencies
 export DEBIAN_FRONTEND=noninteractive
 
+# =============================================
+# Install suppack builder
+# TODO: check PLATFORM_VERSION
+rpm -q supp-pack-build-2.1.0-xs55.noarch
+if [ $? -ne 0 ]; then
+	rpm -i http://coltrane.uk.xensource.com/usr/groups/release/XenServer-7.x/XS-7.0/RTM-125380/binary-packages/RPMS/domain0/RPMS/noarch/supp-pack-build-2.1.0-xs55.noarch.rpm
+fi
+rpm -q xcp-python-libs-1.9.0-159.noarch
+if [ $? -ne 0 ]; then
+	rpm -i http://coltrane.uk.xensource.com/usr/groups/release/XenServer-7.x/XS-7.0/RTM-125380/binary-packages/RPMS/domain0/RPMS/noarch/xcp-python-libs-1.9.0-159.noarch.rpm
+fi
 
 # =============================================
 # Check out rpm packaging repo
@@ -65,14 +75,15 @@ git clone "$NOVA_GITREPO" nova
 cd nova
 git checkout -b mos_nova "$GITBRANCH"
 # patch xenhost as this file is not merged into this release
-cp $DEPLOYMENT_SCRIPT_ROOT/patchset/xenhost plugins/xenserver/xenapi/etc/xapi.d/plugins/
+cp $FUELPLUG_UTILS_ROOT/../plugin_source/deployment_scripts/patchset/xenhost plugins/xenserver/xenapi/etc/xapi.d/plugins/
 cd ..
 
 cp -r xenserver-nova-suppack-builder/plugins/xenserver/xenapi/* nova/plugins/xenserver/xenapi/
 cd nova/plugins/xenserver/xenapi/contrib
 ./build-rpm.sh $XS_PLUGIN_VERSION
-cd $FUELPLUG_UTILS_ROOT/xenserver-suppack/
+cd $FUELPLUG_UTILS_ROOT/
 RPMFILE=$(find -name "openstack-xen-plugins-*.noarch.rpm" -print)
+cd xenserver-suppack
 
 
 # =============================================
@@ -87,23 +98,17 @@ cp -r xenserver-nova-suppack-builder/neutron/* \
       neutron/neutron/plugins/ml2/drivers/openvswitch/agent/xenapi/
 cd neutron/neutron/plugins/ml2/drivers/openvswitch/agent/xenapi/contrib
 ./build-rpm.sh $XS_PLUGIN_VERSION
-cd $FUELPLUG_UTILS_ROOT/xenserver-suppack/
+cd $FUELPLUG_UTILS_ROOT/
+
 NEUTRON_RPMFILE=$(find -name "openstack-neutron-xen-plugins-*.noarch.rpm" -print)
 
 
 # =============================================
 # Create Supplemental pack
 rm -rf suppack
-mkdir suppack
+mkdir -p suppack
 
-DDKROOT=$(mktemp -d)
-
-wget -qO - "$DDK_ROOT_URL" | sudo tar -xzf - -C "$DDKROOT"
-
-sudo mkdir $DDKROOT/mnt/host
-sudo mount --bind $(pwd) $DDKROOT/mnt/host
-
-sudo tee $DDKROOT/buildscript.py << EOF
+tee buildscript.py << EOF
 from xcp.supplementalpack import *
 from optparse import OptionParser
 
@@ -124,14 +129,10 @@ setup(originator='xcp', name='xenapi-plugins-$OS_RELEASE', product='XenServer',
       outdir=options.outdir, output=['iso'])
 EOF
 
-sudo chroot $DDKROOT python buildscript.py \
+python buildscript.py \
 --pdn=xenserverplugins \
 --pdv=$OS_RELEASE \
 --bld=0 \
---out=/mnt/host/suppack \
-/mnt/host/$RPMFILE \
-/mnt/host/$NEUTRON_RPMFILE
-
-# Cleanup
-sudo umount $DDKROOT/mnt/host
-sudo rm -rf "$DDKROOT"
+--out=$FUELPLUG_UTILS_ROOT \
+$FUELPLUG_UTILS_ROOT/$RPMFILE \
+$FUELPLUG_UTILS_ROOT/$NEUTRON_RPMFILE
