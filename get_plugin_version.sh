ad5462d1608e267b49a19722671215de9ff44a33
Bob Ball <bob.ball@citrix.com>
2016-08-26 13:14:09 +0100

    Make branding based on the branch, to remove the PLUGIN_REVISION variable
    By checking which branch we are on, we can automatically determine the
    revision based on the length of history between the root of the branch
    and this change.
    
    Normally (and in development environments) this isn't guaranteed as you can
    create arbitrary branches (e.g. branch a and branch b could have the same
    merge base) but if run against code checked out from github will give a
    consistent version number
    
    Change-Id: I6eff6befce1408dac144dd7dfb3f3055820960a8

---
+++ get_plugin_version.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -eu
+
+# Source the branding file
+. ${1}
+
+# Find shortest delta
+my_merge_base=$(git merge-base HEAD origin/master)
+
+shortest_branch=master
+for branch in $PLUGIN_BRANCHES; do
+
+    # Verify that the named branch actually exists
+    set +e
+    git rev-parse --verify origin/$branch >/dev/null 2>/dev/null
+    branch_test_exit_code=$?
+    set -e
+    if [ $branch_test_exit_code -gt 0 ]; then
+	continue
+    fi
+    
+    branch_merge_base=$(git merge-base origin/master origin/$branch)
+
+    if [ "$branch_merge_base" == "$my_merge_base" ]; then
+	shortest_branch=$branch
+    fi
+done
+
+var_name=PLUGIN_VERSION_${shortest_branch//./_}
+branch_merge_base=$(git merge-base origin/master origin/$shortest_branch)
+branch_major=${!var_name}
+if [ $shortest_branch == 'master' ]; then
+    branch_minor=$(git rev-list HEAD --count)
+else
+    branch_minor=$(git rev-list HEAD ^$branch_merge_base --count)
+fi
+echo $branch_major $branch_minor
